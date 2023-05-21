8116b3ecd2fe72a0738e429b792ceeedeaeca480
aiqiang <aiqiang@tetras.ai>
2023-05-21 08:43:08 +0800

    NEW: function:
    Add new lua Setkernel() for change the tab to space
    
    Signed-off-by: aiqiang <aiqiang@tetras.ai>

--- lua/basic.lua
+++ lua/basic.lua
@@ -14,14 +14,16 @@ vim.wo.cursorline = true
 vim.wo.signcolumn = "yes"
 -- 右侧参考线，超过表示代码太长了，考虑换行
 vim.wo.colorcolumn = 100
+vim.o.shiftround = true
+
 -- 缩进4个空格等于一个Tab
 vim.o.tabstop = 8
 vim.o.softtabstop = 8
-vim.o.shiftround = true
 -- >> << 时移动长度
 vim.o.shiftwidth = 8
 -- 空格替代tab
 vim.o.expandtab = false
+
 -- 新行对齐当前行
 vim.o.autoindent = true
 vim.o.smartindent = true
@@ -76,3 +78,23 @@ vim.o.pumheight = 10
 vim.o.showtabline = 2
 -- 使用增强状态栏插件后不再需要 vim 的模式提示
 vim.o.showmode = false
+
+Setkernel = function()
+    -- 缩进4个空格等于一个Tab
+    vim.o.tabstop = 8
+    vim.o.softtabstop = 8
+    -- >> << 时移动长度
+    vim.o.shiftwidth = 8
+    -- 空格替代tab
+    vim.o.expandtab = false
+end
+
+Setuser = function()
+    -- 缩进4个空格等于一个Tab
+    vim.o.tabstop = 4
+    vim.o.softtabstop = 4
+    -- >> << 时移动长度
+    vim.o.shiftwidth = 4
+    -- 空格替代tab
+    vim.o.expandtab = true
+end
